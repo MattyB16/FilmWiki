082e8fd6bfe28dca56e05ee488920fede2796fbf
Matthew Barker <mbarker@softwareinstitute.com>
2022-02-08 16:58:41 +0000

    4th add all dependencies in pom

---
+++ src/main/java/com/sakilaDB/TSI/matthew/barker/demo/LanguageRepository.java
@@ -0,0 +1,8 @@
+package com.sakilaDB.TSI.matthew.barker.demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface LanguageRepository extends CrudRepository<Language, Integer> {
+
+
+}
