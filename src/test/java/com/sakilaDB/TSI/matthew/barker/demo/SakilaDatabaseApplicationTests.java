825c4ad5c3d4a4f049d7208c3328b11d2124f0f3
Matthew Barker <mbarker@softwareinstitute.com>
2022-02-09 10:48:09 +0000

    film,language,actor,category done

--- src/test/java/com/sakilaDB/TSI/matthew/barker/demo/SakilaDatabaseApplicationTests.java
+++ src/test/java/com/sakilaDB/TSI/matthew/barker/demo/SakilaDatabaseApplicationTests.java
@@ -1,13 +1,13 @@
 package com.sakilaDB.TSI.matthew.barker.demo;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SakilaDatabaseApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SakilaDatabaseApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
