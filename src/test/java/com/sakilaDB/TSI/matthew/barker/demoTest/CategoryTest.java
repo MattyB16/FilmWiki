451c3661753122b3ef368370a2fd2912ac869179
Matthew Barker <mbarker@softwareinstitute.com>
2022-02-11 10:29:59 +0000

    test getters and setters in all classes

--- src/test/java/com/sakilaDB/TSI/matthew/barker/demoTest/CategoryTest.java
+++ src/test/java/com/sakilaDB/TSI/matthew/barker/demoTest/CategoryTest.java
@@ -1,4 +1,27 @@
 package com.sakilaDB.TSI.matthew.barker.demoTest;
 
+import com.sakilaDB.TSI.matthew.barker.demo.Category;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class CategoryTest {
+
+
+    @Test
+    public void testName() {
+
+        Category testName = new Category();
+        testName.setName("Horror");
+        assertEquals("Horror", testName.getName(), "Incorrect name!");
+    }
+
+
+    private Category category = new Category();
+
+    @Test
+    public void test_constructor() {
+        assertTrue(category instanceof Category, "Not an instance of Actor");
+    }
 }
--- src/test/java/com/sakilaDB/TSI/matthew/barker/demoTest/LanguageTest.java
+++ src/test/java/com/sakilaDB/TSI/matthew/barker/demoTest/LanguageTest.java
@@ -1,4 +1,27 @@
 package com.sakilaDB.TSI.matthew.barker.demoTest;
 
+import com.sakilaDB.TSI.matthew.barker.demo.Category;
+import com.sakilaDB.TSI.matthew.barker.demo.Language;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class LanguageTest {
+
+    @Test
+    public void testName() {
+
+        Language testName = new Language();
+        testName.setName("Greek");
+        assertEquals("Greek", testName.getName(), "Incorrect name!");
+    }
+
+
+    private Language language = new Language();
+
+    @Test
+    public void test_constructor() {
+        assertTrue(language instanceof Language, "Not an instance of Actor");
+    }
 }
